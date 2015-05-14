046ff95580f76a8d742fa51c3c3f886c516f8be5
Ben Stephenson <bjames.stephenson@gmail.com>
2015-05-15 00:25:59 +0100

    Move finding the correct game to a function in the resources class.

--- src/main/scala/com/spaceape/hiring/NoughtsResource.scala
+++ src/main/scala/com/spaceape/hiring/NoughtsResource.scala
@@ -15,43 +15,55 @@ class NoughtsResource() {
 	//eurgh...
 	var gameList = Array[Game]()
 
+	val predGameById: (Game, String) => Boolean = (game, gameId) => {
+		game.getId == gameId
+	}
+
+	val predGameWithPlayerCombo: (Game, String, String) => Boolean = (game, p1, p2) => {
+			(game.player1 == p1 && game.player2 == p2) ||
+			(game.player1 == p2 && game.player2 == p1)
+	}
+
   @POST
   def createGame(@QueryParam("player1Id") player1: String, @QueryParam("player2Id") player2: String): Response = {
-		val game = new Game(player1, player2)
 
-		gameList.foreach { g => println(g.getId) }
-
-		gameList.find { g =>
-			(g.player1 == player1 && g.player2 == player2) ||
-			(g.player1 == player2 && g.player2 == player1)
-		} match {
-			case Some(existing) => {
-				println(s"Found existing game ${existing.player1} ${existing.player2}")
-				Response.status(Status.FORBIDDEN).build
-			}
-			case None => {
-				gameList = gameList :+ game
-				Response.ok(game.getId).build
+		val closure: (Option[Game]) => Response = (game) => {
+			game match {
+				case Some(existing) => {
+					println(s"Found existing game ${existing.player1} ${existing.player2}")
+					Response.status(Status.FORBIDDEN).build
+				}
+				case None => {
+					val newGame = new Game(player1, player2)
+					gameList = gameList :+ newGame
+					Response.ok(newGame.getId).build
+				}
 			}
 		}
+
+		findGameAndExecute(predGameWithPlayerCombo(_, player1, player2), closure)
   }
+
 
   @GET
   @Path("/{gameId}")
-  def getGame(@PathParam("gameId") gameId: String): GameState = {
-		gameList.find {
-			_.getId == gameId
-		} match {
-			case Some(currentGame) => {
-				val state = currentGame.getState
-				if (state.gameOver) {
-					gameList = gameList diff List(currentGame)
-				}
+  def getGame(@PathParam("gameId") gameId: String): Response = {
 
-				state
+		val closure: (Option[Game]) => Response = (game) => {
+			game match {
+				case Some(currentGame) => {
+					val state = currentGame.getState
+					if (state.gameOver) {
+						gameList = gameList diff List(currentGame)
+					}
+
+					Response.ok(state).build
+				}
+				case None => Response.ok(new GameState(Some("1"), true)).build
 			}
-			case None => new GameState(Some("1"), true)
 		}
+
+		findGameAndExecute(predGameById(_, gameId), closure)
   }
 
 
@@ -59,14 +71,23 @@ class NoughtsResource() {
   @Path("/{gameId}")
   def makeMove(@PathParam("gameId") gameId: String, move: Move): Response = {
 
-		val currentGame = gameList.find {
-			_.getId == gameId
+		val closure: (Option[Game]) => Response = (game) => {
+			game match {
+				case Some(game) => game.applyMove(move) match {
+					case true  => Response.status(Status.ACCEPTED).build
+					case false => Response.status(Status.FORBIDDEN).build
+				}
+					case _ => Response.status(Status.NOT_FOUND).build
+			}
 		}
 
-		if (currentGame.get.applyMove(move))
-			Response.status(Status.ACCEPTED).build
-		else
-			Response.status(Status.FORBIDDEN).build
+		findGameAndExecute(predGameById(_, gameId), closure)
   }
+
+	def findGameAndExecute(predicate: (Game) => Boolean,
+		closure: (Option[Game]) => Response): Response = {
+		val game = gameList.find { predicate(_) }
+		closure(game)
+	}
 }
 //  vim: set ts=2 sw=2 tw=78 fdm=marker noet :
